46a04d0ae6a5b776ce635faf396e1faa6c0c6e1d
Ibrahim Riyami <iariyami@ncsu.edu>
2020-10-08 09:53:04 -0400

    controller routes defined

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -23,27 +23,8 @@ router.post("/api/burger", function (req, res) {
 });
 
 router.put("/api/burger/:id", function (req, res) {
-    burger.update(req.body.sleepy
-    }, condition, function (result) {
-    if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-    } else {
-        res.status(200).end();
-    }
-});
-});
-
-router.delete("/api/cats/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
-    cat.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+    burger.update(req.body.id, function (data) {
+        res.json(data);
     });
 });
 
