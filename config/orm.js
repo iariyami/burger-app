16550f1ba27bdffa180e2bdce356a43ce38bd634
Ibrahim Riyami <iariyami@ncsu.edu>
2020-10-08 10:57:45 -0400

    backend touches

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,7 @@ const connection = require("../config/connection.js");
 
 const orm = {
     selectAll: function (cb) {
-        var queryString = "SELECT * FROM " + burgers;
+        var queryString = "SELECT * FROM burgers";
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
